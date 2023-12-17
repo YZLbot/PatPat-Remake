8eb1e29c495dcdf55fea06d71cdeca65d69b0b6c
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2023-12-17 10:38:03 +0800

    Overflow compatible

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,35 @@
 plugins {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.8.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.9.0-M1"
+    id("net.mamoe.mirai-console") version "2.16.0";
 }
 
 group = "org.laolittle.plugin"
 version = "2.0.1"
 
 repositories {
-    maven("https://maven.aliyun.com/repository/public")
+    if (System.getenv("CI")?.toBoolean() != true) {
+        maven("https://maven.aliyun.com/repository/public")
+    }
     mavenCentral()
-    mavenLocal()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
+
+mirai {
+    noTestCore = true
+    setupConsoleTestRuntime {
+        classpath = classpath.filter {
+            !it.nameWithoutExtension.startsWith("mirai-core-jvm")
+        }
+    }
+}
+
+dependencies {
+    val overflowVersion = "2.16.0-00fd581-SNAPSHOT"
+    compileOnly("top.mrxiaom:overflow-core-api:$overflowVersion")
+    testConsoleRuntime("top.mrxiaom:overflow-core:$overflowVersion")
 }
 
 tasks {
